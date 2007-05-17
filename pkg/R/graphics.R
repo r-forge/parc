2623abeddba8567d015c25be90258725e3915961
stefan7th (Stefan Theussl) <stefan.theussl@R-project.org>
2007-05-17 22:27:26 +0000

    graphics.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/parc@24 766be6ca-ae6d-431a-b1f1-f339bbcdd0be

---
+++ pkg/R/graphics.R
@@ -0,0 +1,15 @@
+## Graphics.R -- package paRc
+## Stefan Theussl
+
+require("vcd")
+
+## palette code from zeileis, hornik (hcl-based color palletes
+## pal <- function(col, border = "light gray", ...) {
+## n <- length(col)
+## plot(0, 0, type="n", xlim = c(0,1),ylim=c(0,1),axes=FALSE,xlab="",ylab="", ...)
+## rect(0:(n-1)/n, 0, 1:n/n, 1, col = col, border = border)
+## }
+## This palettes are good
+## pal(rainbow_hcl(10, c=80, l=65, start = 0, end = 360), main = "dynamic_bt")
+## pal(rainbow_hcl(10, c=80, l=65, start = 20, end = 340), main = "dynamic_bt")
+
