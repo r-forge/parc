ce7fe1cbad2732e2718b7f84a01ea0d8b9f3ae93
stefan7th (Stefan Theussl) <stefan.theussl@R-project.org>
2007-08-16 21:45:57 +0000

    Thesis: R HPC chapter
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/parc@85 766be6ca-ae6d-431a-b1f1-f339bbcdd0be

--- work/thesis_stef/examples/PVM/distributed-mm.R
+++ work/thesis_stef/examples/PVM/distributed-mm.R
@@ -9,7 +9,7 @@ task <- "matrix multiplication"
 taskID <- "mm"
 paradigm <- "distributed"
 types <- c("PVM","snow-PVM","PVM-wB")
-complexity <- c(1000,2500,5000)
+complexity <- c(5000)
 
 bm <- create.benchmark(task=task, data=list(),
                        type=types[1], parallel=TRUE, cpu_range=1:maxcpu)
@@ -22,6 +22,7 @@ for(n in complexity){
   bm.data(bm) <- bmdata
   for(type in types){
     bm.type(bm) <- type
+    writeLines(paste("Starting",type,"benchmark with complexity",n,"..."))
     results <- run.benchmark(bm) 
 
     save(results,file=paste(paste(paradigm,taskID,type,n,sep="-"),".Rda",sep=""))
