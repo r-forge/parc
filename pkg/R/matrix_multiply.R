54f1a8cbc96f9125693354df12f0b6b3d877e9f3
stefan7th (Stefan Theussl) <stefan.theussl@R-project.org>
2008-06-11 23:26:30 +0000

    added new version of OMP matrix multiplication; GCC OMP support
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/parc@136 766be6ca-ae6d-431a-b1f1-f339bbcdd0be

--- pkg/R/matrix_multiply.R
+++ pkg/R/matrix_multiply.R
@@ -1,20 +1,21 @@
 ##########################################################
 ## R Package: paRc
 ## matrix_multiply.R
-## application: Parallel Matrix Multiplication
-## theussl, 2007
+## Application: Serial and shared as well as distributed
+## memory parallel matrix multiplication
+## theussl, 2007, 2008
 ##########################################################
 
 ## input validation function
 matrix_mult_validate <- function(X, Y, dx, dy){
-  if(!(is.matrix(X) && is.matrix(Y)))
-    stop("'X' and 'Y' must be matrices.")
+  if( ! (is.matrix(X) && is.matrix(Y)) )
+    stop( "'X' and 'Y' must be matrices." )
   
-  if(!(dx[2]==dy[1]))
-    stop("'X' and 'Y' not compatible")
+  if( ! (dx[2] == dy[1]) )
+    stop( "'X' and 'Y' not compatible" )
 }
 
-## serial version of matrix multiplication
+## serial version of matrix multiplication implemented in C (O(n^3))
 serial_matrix_multiplication <- function(X, Y)
 {
   dx <- dim(X) ## dimensions of matrix A
@@ -30,9 +31,33 @@ serial_matrix_multiplication <- function(X, Y)
   out <- .C("Serial_matrix_mult",
             x, as.integer(dx[1]), as.integer(dx[2]),
             y, as.integer(dy[1]), as.integer(dy[2]),
-            z = double(dx[1]*dy[2]),
+            z = double(dx[1] * dy[2]),
             PACKAGE = "paRc")
-  matrix(out$z,ncol=dy[2])
+  matrix(out$z, ncol = dy[2])
+}
+
+## matrix multiplication, partially using dgemm from the BLAS, i.e. for
+## the inner most loop (the dot product of row and column vevtors)
+matrix_multiplication_BLAS <- function(X, Y, n_cpu = 1)
+{
+  
+  dx <- dim(X) ## dimensions of matrix A
+  dy <- dim(Y) ## dimensions of matrix B
+  ## input validation
+  matrix_mult_validate(X, Y, dx, dy)
+  ## data preparation
+  ## we need the transposed matrix of X to allow a simple usage of
+  ## dgemm. we need rows of X accessible in a sequence in an array.
+  x <- as.vector(t(X))
+  storage.mode(x) <- "double"
+  y <- as.vector(Y)
+  storage.mode(y) <- "double"
+  out <- .C("matrix_mult_BLAS",
+            x, as.integer(dx[1]), as.integer(dx[2]),
+            y, as.integer(dy[1]), as.integer(dy[2]),
+            z = double(dx[1] * dy[2]),
+            PACKAGE = "paRc")
+  matrix(out$z, ncol = dy[2])
 }
 
 
@@ -55,32 +80,64 @@ omp_matrix_multiplication <- function(X, Y, n_cpu = 1)
   out <- .C("OMP_matrix_mult",
             x, as.integer(dx[1]), as.integer(dx[2]),
             y, as.integer(dy[1]), as.integer(dy[2]),
-            z = double(dx[1]*dy[2]),
+            z = double(dx[1] * dy[2]),
             PACKAGE = "paRc")
-  matrix(out$z,ncol=dy[2])
+  matrix(out$z, ncol = dy[2])
 }
 
+omp_matrix_multiplication_BLAS <- function(X, Y, n_cpu = 1)
+{
+  
+  dx <- dim(X) ## dimensions of matrix A
+  dy <- dim(Y) ## dimensions of matrix B
+  ## input validation
+  matrix_mult_validate(X, Y, dx, dy)
+  ## set number of cpus to use
+  omp_set_num_threads(n_cpu)
+  ## data preparation
+    ## we need the transposed matrix of X to allow a simple usage of
+  ## dgemm. we need rows of X accessible in a sequence in an array.
+  x <- as.vector(t(X))
+  storage.mode(x) <- "double"
+  y <- as.vector(Y)
+  storage.mode(y) <- "double"
+  ## C call of parallel matrix multiply using openMP  
+  out <- .C("OMP_matrix_mult_BLAS",
+            x, as.integer(dx[1]), as.integer(dx[2]),
+            y, as.integer(dy[1]), as.integer(dy[2]),
+            z = double(dx[1] * dy[2]),
+            PACKAGE = "paRc")
+  matrix(out$z, ncol = dy[2])
+}
+
+
 mm.Rmpi.slave <- function(){
-  commrank <- mpi.comm.rank() -1
-  if(commrank==(n_cpu - 1))
-    local_mm <- X[(nrows_on_slaves*commrank + 1):(nrows_on_slaves*commrank + nrows_on_last),]%*%Y
+  commrank <- mpi.comm.rank() - 1
+  if( commrank == (n_cpu - 1) )
+    local_mm <- X[(nrows_on_slaves * commrank + 1):
+                  (nrows_on_slaves * commrank + nrows_on_last),] %*% Y
   else
-    local_mm <- X[(nrows_on_slaves*commrank + 1):(nrows_on_slaves*commrank + nrows_on_slaves),]%*%Y
-  mpi.gather.Robj(local_mm,root=0,comm=1)    
+    local_mm <- X[(nrows_on_slaves * commrank + 1):
+                  (nrows_on_slaves * commrank + nrows_on_slaves),] %*% Y
+  mpi.gather.Robj(local_mm, root = 0, comm = 1)    
 }
 
 mm.Rmpi.slave.C <- function(){
   require("paRc")
-  commrank <- mpi.comm.rank() -1
-  if(commrank==(n_cpu - 1))
-    local_mm <- serial_matrix_multiplication(X[(nrows_on_slaves*commrank + 1):(nrows_on_slaves*commrank + nrows_on_last),],Y)
+  commrank <- mpi.comm.rank() - 1
+  if( commrank == (n_cpu - 1) )
+    local_mm <- serial_matrix_multiplication(
+                  X[(nrows_on_slaves * commrank + 1):
+                    (nrows_on_slaves * commrank + nrows_on_last), ], Y)
   else
-    local_mm <- serial_matrix_multiplication(X[(nrows_on_slaves*commrank + 1):(nrows_on_slaves*commrank + nrows_on_slaves),],Y)
-  mpi.gather.Robj(local_mm,root=0,comm=1)    
+    local_mm <- serial_matrix_multiplication(
+                  X[(nrows_on_slaves * commrank + 1):
+                    (nrows_on_slaves * commrank + nrows_on_slaves), ], Y)
+  mpi.gather.Robj(local_mm, root = 0, comm = 1)    
 }
 
 ## master job
-mm.Rmpi <- function(X, Y, n_cpu = 1, spawnRslaves=FALSE) {
+mm.Rmpi <- function(X, Y, n_cpu = 1, spawnRslaves = FALSE) {
   dx <- dim(X) ## dimensions of matrix A
   dy <- dim(Y) ## dimensions of matrix B
   ## Input validation
@@ -96,8 +153,8 @@ mm.Rmpi <- function(X, Y, n_cpu = 1, spawnRslaves=FALSE) {
   mpi.bcast.Robj2slave(X) 
   mpi.bcast.Robj2slave(n_cpu)
   
-  nrows_on_slaves <- ceiling(dx[1]/n_cpu)
-  nrows_on_last <- dx[1] - (n_cpu - 1)*nrows_on_slaves
+  nrows_on_slaves <- ceiling(dx[1] / n_cpu)
+  nrows_on_last <- dx[1] - (n_cpu - 1) * nrows_on_slaves
   mpi.bcast.Robj2slave(nrows_on_slaves)
   mpi.bcast.Robj2slave(nrows_on_last)
   mpi.bcast.Robj2slave(mm.Rmpi.slave)
@@ -107,19 +164,20 @@ mm.Rmpi <- function(X, Y, n_cpu = 1, spawnRslaves=FALSE) {
 
   ## gather partial results from slaves
   local_mm <- NULL
-  mm <- mpi.gather.Robj(local_mm, root=0, comm=1)
+  mm <- mpi.gather.Robj(local_mm, root = 0, comm = 1)
   out <- NULL
 
-  ## Rmpi returns a list when the vectors have different length (local_mm = NULL)
+  ## Rmpi returns a list when the vectors have different length
+  ## (local_mm = NULL)
   for(i in 1:n_cpu)
-    out <- rbind(out,mm[[i+1]])
+    out <- rbind(out, mm[[i+1]])
   
   if( spawnRslaves == TRUE )
     mpi.close.Rslaves()
   out
 }
 
-mm.Rmpi.C <- function(X, Y, n_cpu = 1, spawnRslaves=FALSE) {
+mm.Rmpi.C <- function(X, Y, n_cpu = 1, spawnRslaves = FALSE) {
   dx <- dim(X) ## dimensions of matrix A
   dy <- dim(Y) ## dimensions of matrix B
   ## Input validation
@@ -135,8 +193,8 @@ mm.Rmpi.C <- function(X, Y, n_cpu = 1, spawnRslaves=FALSE) {
   mpi.bcast.Robj2slave(X) 
   mpi.bcast.Robj2slave(n_cpu)
   
-  nrows_on_slaves <- ceiling(dx[1]/n_cpu)
-  nrows_on_last <- dx[1] - (n_cpu - 1)*nrows_on_slaves
+  nrows_on_slaves <- ceiling(dx[1] / n_cpu)
+  nrows_on_last <- dx[1] - (n_cpu - 1) * nrows_on_slaves
   mpi.bcast.Robj2slave(nrows_on_slaves)
   mpi.bcast.Robj2slave(nrows_on_last)
   mpi.bcast.Robj2slave(mm.Rmpi.slave.C)
@@ -146,12 +204,13 @@ mm.Rmpi.C <- function(X, Y, n_cpu = 1, spawnRslaves=FALSE) {
 
   ## gather partial results from slaves
   local_mm <- NULL
-  mm <- mpi.gather.Robj(local_mm, root=0, comm=1)
+  mm <- mpi.gather.Robj(local_mm, root = 0, comm = 1)
   out <- NULL
 
-  ## Rmpi returns a list when the vectors have different length (local_mm = NULL)
+  ## Rmpi returns a list when the vectors have different length
+  ## (local_mm = NULL)
   for(i in 1:n_cpu)
-    out <- rbind(out,mm[[i+1]])
+    out <- rbind(out, mm[[i+1]])
   
   if( spawnRslaves == TRUE )
     mpi.close.Rslaves()
@@ -170,26 +229,26 @@ mm.rpvm <- function(X, Y, n_cpu = 1) {
   WORKTAG <- 17
   RESULTAG <- 82
   
-  if(n_cpu == 1)
+  if( n_cpu == 1 )
     return(X%*%Y)
 
   mytid <- .PVM.mytid()
   children <- .PVM.spawnR(ntask = n_cpu, slave = "mm_slave.R")
-  if (all(children < 0)) {
+  if ( all(children < 0) ) {
     cat("Failed to spawn any task: ", children, "\n")
     .PVM.exit()
   }
-  else if (any(children < 0)) {
+  else if ( any(children < 0) ) {
     cat("Failed to spawn some tasks.  Successfully spawned ",
         sum(children > 0), "tasks\n")
     children <- children[children > 0]
   }
 
-  nrows_on_slaves <- ceiling(dx[1]/n_cpu)
-  nrows_on_last <- dx[1] - (n_cpu - 1)*nrows_on_slaves
+  nrows_on_slaves <- ceiling(dx[1] / n_cpu)
+  nrows_on_last <- dx[1] - (n_cpu - 1) * nrows_on_slaves
 
   ## distribute data
-  for (id in 1:length(children)) {
+  for(id in 1:length(children)) {
     .PVM.initsend()
     .PVM.pkint(id)
     .PVM.pkint(n_cpu)
@@ -198,7 +257,7 @@ mm.rpvm <- function(X, Y, n_cpu = 1) {
     .PVM.pkdblmat(X)
     .PVM.pkdblmat(Y)
     .PVM.send(children[id], WORKTAG)
-   }
+  }
 
   ## receive partial results
   partial.results <- list()
@@ -211,7 +270,7 @@ mm.rpvm <- function(X, Y, n_cpu = 1) {
   ## return in matrix form
   out <- NULL
   for(i in 1:n_cpu)
-    out <- rbind(out,partial.results[[i]])
+    out <- rbind(out, partial.results[[i]])
   out
 }
 
@@ -221,29 +280,29 @@ mm.rpvm.C <- function(X, Y, n_cpu = 1) {
   dy <- dim(Y) ## dimensions of matrix B
 
   ## Input validation
-  matrix_mult_validate(X,Y,dx,dy)
+  matrix_mult_validate(X, Y, dx, dy)
 
   ## Tags for message sending
   WORKTAG <- 17
   RESULTAG <- 82
   
   if(n_cpu == 1)
-    return(serial_matrix_multiplication(X,Y))
+    return(serial_matrix_multiplication(X, Y))
 
   mytid <- .PVM.mytid()
   children <- .PVM.spawnR(ntask = n_cpu, slave = "mm_slaveC.R")
-  if (all(children < 0)) {
+  if ( all(children < 0) ) {
     cat("Failed to spawn any task: ", children, "\n")
     .PVM.exit()
   }
-  else if (any(children < 0)) {
+  else if ( any(children < 0) ) {
     cat("Failed to spawn some tasks.  Successfully spawned ",
         sum(children > 0), "tasks\n")
     children <- children[children > 0]
   }
 
-  nrows_on_slaves <- ceiling(dx[1]/n_cpu)
-  nrows_on_last <- dx[1] - (n_cpu - 1)*nrows_on_slaves
+  nrows_on_slaves <- ceiling(dx[1] / n_cpu)
+  nrows_on_last <- dx[1] - (n_cpu - 1) * nrows_on_slaves
 
   ## distribute data
   for (id in 1:length(children)) {
@@ -255,11 +314,11 @@ mm.rpvm.C <- function(X, Y, n_cpu = 1) {
     .PVM.pkdblmat(X)
     .PVM.pkdblmat(Y)
     .PVM.send(children[id], WORKTAG)
-   }
-
+  }
+  
   ## receive partial results
   partial.results <- list()
-  for (child in children) {
+  for(child in children) {
     .PVM.recv(-1, RESULTAG)
     rank <- .PVM.upkint()
     partial.results[[rank]] <- .PVM.upkdblmat()
@@ -268,13 +327,13 @@ mm.rpvm.C <- function(X, Y, n_cpu = 1) {
   ## return in matrix form
   out <- NULL
   for(i in 1:n_cpu)
-    out <- rbind(out,partial.results[[i]])
+    out <- rbind(out, partial.results[[i]])
   out
 }
 
 serial_dot_product <- function(x, y)
 {
-    if(!is.vector(x) && !is.vector(y))
+    if( ! is.vector(x) && ! is.vector(y) )
         stop("'x' and 'y' must be vectors.")
 
     nx <- length(x)
@@ -289,4 +348,3 @@ serial_dot_product <- function(x, y)
               PACKAGE = "paRc")
     out$prod
 }
-
--- pkg/src/matrix_mult.c
+++ pkg/src/matrix_mult.c
@@ -3,11 +3,12 @@
 #ifdef _OPENMP
 #include <omp.h>
 #endif
+// the following includes R dgemm function definitions
+#include <R_ext/Applic.h>
 
 /*
  * Matrix Multiplication
  */
-
 
 void Serial_matrix_mult( double *x, int *nrx, int *ncx,
 			 double *y, int *nry, int *ncy,
@@ -24,11 +25,28 @@ void Serial_matrix_mult( double *x, int *nrx, int *ncx,
     }
 }
 
+void OMP_matrix_mult_BLAS( double *x, int *nrx, int *ncx,
+			   double *y, int *nry, int *ncy,
+			   double *z) {
+  int i, j, k;
+  char *transa = "N", *transb = "N";
+  double one = 1.0, zero = 0.0;
+  int ione = 1;
+  
+#pragma omp parallel for shared(x, y, z, j, k, nrx, nry, ncy, ncx)
+  for(i = 0; i < *nrx; i++)
+    for(j = 0; j < *ncy; j++){
+       F77_CALL(dgemm)(transa, transb, &ione, &ione, 
+		       ncx, &one, &x[i**ncx], &ione, &y[j**nry], 
+		       nry, &zero, &z[i + j**nrx], &ione);
+    }
+}
+
 void OMP_matrix_mult( double *x, int *nrx, int *ncx,
 		      double *y, int *nry, int *ncy,
 		      double *z) {
   int i, j, k;
-  double tmp, sum;
+  double sum;
 
 #pragma omp parallel for private(sum) shared(x, y, z, j, k, nrx, nry, ncy, ncx)
   for(i = 0; i < *nrx; i++)
@@ -38,5 +56,35 @@ void OMP_matrix_mult( double *x, int *nrx, int *ncx,
 	sum += x[i + k**nrx]*y[k + j**nry];
       z[i + j**nrx] = sum;
     }
+}
+
+void matrix_mult_BLAS( double *x, int *nrx, int *ncx,
+		       double *y, int *nry, int *ncy,
+		       double *z) {
+  int i, j, k;
+  char *transa = "N", *transb = "N";
+  double one = 1.0, zero = 0.0;
+  int ione = 1;
+  
+  // #pragma omp parallel for private(sum) shared(x, y, z, j, k, nrx, nry, ncy, ncx)
+  for(i = 0; i < *nrx; i++)
+    for(j = 0; j < *ncy; j++){
+       F77_CALL(dgemm)(transa, transb, &ione, &ione, 
+		       ncx, &one, &x[i**ncx], &ione, &y[j**nry], 
+		       nry, &zero, &z[i + j**nrx], &ione);
+    }
+}
+
+void dot_product_BLAS( double *x, int *nx, 
+		       double *y, int *ny, 
+		       double *z) {
+  // transx/y specifies the form of A, either N -> A or T -> A'
+  char *transa = "N", *transb = "N";
+  double one = 1.0, zero = 0.0;
+  int ione = 1;
+  F77_CALL(dgemm)(transa, transb, &ione, &ione, nx, &one,
+                            x, &ione, y, ny, &zero, z, &ione);
+  
+  
 }
 
